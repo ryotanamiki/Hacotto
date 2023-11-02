31f5379aaf5aa9f706a7633a1bb9e1b5b67a5bf8
ryotanamiki <aaasti555@gmail.com>
2023-11-02 21:51:25 +0900

    Done

---
+++ js/script-3.js
@@ -0,0 +1,36 @@
+     HTML CSS JS 
+
+    Result
+    Skip Results Iframe
+
+EDIT ON
+
+// When the browser is ready...
+  $(function() {
+    // validate
+    $("#contact").validate({
+        // Set the validation rules
+        rules: {
+            name: "required",
+            email: {
+                required: true,
+                email: true
+            },
+            message: "required",
+        },
+        // Specify the validation error messages
+        messages: {
+            name: "Please enter your name",
+            email: "Please enter a valid email address",
+            message: "Please enter a message",
+        },
+        // submit handler
+        submitHandler: function(form) {
+          //form.submit();
+           $(".message").show();
+           $(".message").fadeOut(4500);
+        }
+    });
+  });
+
+// JavaScript Document
